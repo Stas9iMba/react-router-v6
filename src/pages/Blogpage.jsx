50d35c966277d13c56e8d20746753e748aa84d44
Stas <46508483+Stas9iMba@users.noreply.github.com>
2023-07-07 03:36:36 +0700

    Параметры поиска

--- src/pages/Blogpage.jsx
+++ src/pages/Blogpage.jsx
@@ -1,8 +1,14 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
+import { BlockFilter } from "../components/BlockFilter";
 
 function Blogpage() {
   const [posts, setPosts] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const postQuery = searchParams.get("post") || "";
+  const latest = searchParams.has("latest");
+  const startsForm = latest ? 80 : 1;
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
@@ -11,18 +17,30 @@ function Blogpage() {
         setPosts(data);
       });
   }, []);
+
   return (
     <div className="container">
       <h1>Our news</h1>
+      <BlockFilter
+        postQuery={postQuery}
+        latest={latest}
+        setSearchParams={setSearchParams}
+      />
       <Link to={"/blog/new"}>Add new post</Link>
       <div>
-        {posts.map((post) => {
-          return (
-            <Link to={`/blog/${post.id}`} key={post.id}>
-              <li>{post.title}</li>
-            </Link>
-          );
-        })}
+        {posts
+          .filter(
+            (post) =>
+              post.title.toLowerCase().includes(postQuery.toLowerCase()) &&
+              post.id >= startsForm
+          )
+          .map((post) => {
+            return (
+              <Link to={`/blog/${post.id}`} key={post.id}>
+                <li>{post.title}</li>
+              </Link>
+            );
+          })}
       </div>
     </div>
   );
